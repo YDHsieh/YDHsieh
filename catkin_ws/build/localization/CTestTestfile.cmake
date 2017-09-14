443f842aaf28158595c3e82e96ed7d2b1655657c
YDHsieh <j91148700@gmail.com>
2017-09-14 17:00:27 +0800

    Add files via upload

---
+++ catkin_ws/build/localization/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yard/duckietown/catkin_ws/src/localization
+# Build directory: /home/yard/duckietown/catkin_ws/build/localization
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(_ctest_localization_rostest_tests_localization_tester_node.test "/home/yard/duckietown/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/yard/duckietown/catkin_ws/build/test_results/localization/rostest-tests_localization_tester_node.xml" "--return-code" "/opt/ros/indigo/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/yard/duckietown/catkin_ws/src/localization --package=localization --results-filename tests_localization_tester_node.xml --results-base-dir \"/home/yard/duckietown/catkin_ws/build/test_results\" /home/yard/duckietown/catkin_ws/src/localization/tests/localization_tester_node.test ")
---
+++ catkin_ws/build/mdoap/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yard/duckietown/catkin_ws/src/mdoap
+# Build directory: /home/yard/duckietown/catkin_ws/build/mdoap
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
