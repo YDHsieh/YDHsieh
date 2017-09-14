8c8e31a8bb9c9b81fd61e595988d68ae7aa696b0
YDHsieh <j91148700@gmail.com>
2017-09-14 17:19:00 +0800

    Add files via upload

---
+++ catkin_ws/build/rostest_example/CMakeFiles/run_tests_rostest_example_rostest_tests_average_quacks_tester_node.test.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/run_tests_rostest_example_rostest_tests_average_quacks_tester_node.test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/run_tests_rostest_example_rostest_tests_average_quacks_tester_node.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ catkin_ws/build/rostest_example/CMakeFiles/run_tests_rostest_example_rostest_tests_quacker_tester.test.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/run_tests_rostest_example_rostest_tests_quacker_tester.test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/run_tests_rostest_example_rostest_tests_quacker_tester.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
