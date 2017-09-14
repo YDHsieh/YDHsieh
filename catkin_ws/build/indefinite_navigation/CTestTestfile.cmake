508f208fee725a0239f440fec2b1f2bd86c06aa8
YDHsieh <j91148700@gmail.com>
2017-09-14 16:57:35 +0800

    Add files via upload

---
+++ catkin_ws/build/indefinite_navigation/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yard/duckietown/catkin_ws/src/indefinite_navigation
+# Build directory: /home/yard/duckietown/catkin_ws/build/indefinite_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(_ctest_indefinite_navigation_rostest_tests_calibrate_turn.test "/home/yard/duckietown/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/indigo/share/catkin/cmake/test/run_tests.py" "/home/yard/duckietown/catkin_ws/build/test_results/indefinite_navigation/rostest-tests_calibrate_turn.xml" "--return-code" "/opt/ros/indigo/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/yard/duckietown/catkin_ws/src/indefinite_navigation --package=indefinite_navigation --results-filename tests_calibrate_turn.xml --results-base-dir \"/home/yard/duckietown/catkin_ws/build/test_results\" /home/yard/duckietown/catkin_ws/src/indefinite_navigation/tests/calibrate_turn.test ")
